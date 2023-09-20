0d01e5ba3145d596de52e83ae965379baa8280c3
ukaoha <ukaohachizoba6@gmail.com>
2023-09-20 10:17:03 +0100

    commit

--- src/Components/ImageGallery/Gallary.tsx
+++ src/Components/ImageGallery/Gallary.tsx
@@ -68,7 +68,7 @@
 // export default Gallery;
 
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { ImageData, imageData } from '../../data';
 import ImageCard from './ImgCard';
 import './imageGallery.css';
@@ -77,10 +77,8 @@ const Gallery: React.FC = () => {
   const [images, setImages] = useState<ImageData[]>(imageData);
   const [searchTerm, setSearchTerm] = useState('');
 
-  const [draggedImageId, setDraggedImageId] = useState<string | null>(null);
-
   const handleDragStart = (e: React.DragEvent<HTMLDivElement>, image: ImageData) => {
-    setDraggedImageId(image.id);
+    e.dataTransfer.setData('imageId', image.id);
   };
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
@@ -89,96 +87,63 @@ const Gallery: React.FC = () => {
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
+    const droppedImageId = e.dataTransfer.getData('imageId');
 
-    if (draggedImageId) {
+    const updatedImages = images.map((image) => {
+      if (image.id === droppedImageId) {
+        return { ...image, position: undefined }; // Reset the position
+      }
+      return image;
+    });
+
+    setImages(updatedImages);
+  };
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>, image: ImageData) => {
+    e.preventDefault();
+    const touch = e.touches[0];
+    const target = e.currentTarget as HTMLDivElement;
+    target.setAttribute('data-imageId', image.id);
+    target.setAttribute('data-touchstartX', String(touch.clientX));
+    target.setAttribute('data-touchstartY', String(touch.clientY));
+  };
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const touch = e.touches[0];
+    const target = e.currentTarget as HTMLDivElement;
+    const imageId = target.getAttribute('data-imageId');
+    const touchstartX = parseFloat(target.getAttribute('data-touchstartX') || '0');
+    const touchstartY = parseFloat(target.getAttribute('data-touchstartY') || '0');
+
+    if (imageId) {
+      const deltaX = touch.clientX - touchstartX;
+      const deltaY = touch.clientY - touchstartY;
+      target.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+    }
+  };
+
+  const handleTouchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const target = e.currentTarget as HTMLDivElement;
+    const imageId = target.getAttribute('data-imageId');
+
+    if (imageId) {
       const updatedImages = images.map((image) => {
-        if (image.id === draggedImageId) {
-          return { ...image, position: Date.now() };
+        if (image.id === imageId) {
+          return { ...image, position: undefined }; // Reset the position
         }
         return image;
       });
 
-      updatedImages.sort((a, b) => (a.position || 0) - (b.position || 0));
-
       setImages(updatedImages);
-      setDraggedImageId(null);
-    }
-  };
-
-  const handleTouchStart = (e: TouchEvent) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    const touch = e.touches[0];
-    const target = e.currentTarget as HTMLDivElement; // Type casting
-
-    target.style.cursor = 'grabbing';
-    target.style.transform = 'scale(1.1)';
-    target.style.zIndex = '999';
-    target.style.transition = 'transform 0.2s';
-    target.style.transformOrigin = 'center';
-    target.style.border = '2px solid #333';
-
-    const offsetX = touch.clientX - target.getBoundingClientRect().left;
-    const offsetY = touch.clientY - target.getBoundingClientRect().top;
-
-    target.dataset.offsetX = offsetX.toString();
-    target.dataset.offsetY = offsetY.toString();
-
-    setDraggedImageId(target.getAttribute('data-image-id'));
-  };
-
-  const handleTouchMove = (e: TouchEvent) => {
-    e.preventDefault();
-
-    if (draggedImageId) {
-      const touch = e.touches[0];
-      const target = e.currentTarget as HTMLDivElement; // Type casting
-
-      const offsetX = parseFloat(target.dataset.offsetX || '0');
-      const offsetY = parseFloat(target.dataset.offsetY || '0');
-
-      const left = touch.clientX - offsetX;
-      const top = touch.clientY - offsetY;
-
-      target.style.left = left + 'px';
-      target.style.top = top + 'px';
-    }
-  };
-
-  const handleTouchEnd = (e: TouchEvent) => {
-    e.preventDefault();
-
-    if (draggedImageId) {
-      const target = e.currentTarget as HTMLDivElement; // Type casting
-
-      target.style.cursor = 'grab';
-      target.style.transform = 'scale(1)';
-      target.style.zIndex = 'auto';
-      target.style.transition = '';
-      target.style.transformOrigin = '';
-      target.style.border = '';
-
-      setDraggedImageId(null);
+      target.style.transform = 'translate(0px, 0px)';
     }
   };
 
   const filteredImages = images.filter((image) =>
     image.tags.some((tag) => tag.toLowerCase().includes(searchTerm.toLowerCase()))
   );
-
-  useEffect(() => {
-    const imageGrid = document.querySelector('.image-grid') as HTMLDivElement;
-    imageGrid.addEventListener('touchstart', handleTouchStart, { passive: false });
-    imageGrid.addEventListener('touchmove', handleTouchMove, { passive: false });
-    imageGrid.addEventListener('touchend', handleTouchEnd, { passive: false });
-
-    return () => {
-      imageGrid.removeEventListener('touchstart', handleTouchStart);
-      imageGrid.removeEventListener('touchmove', handleTouchMove);
-      imageGrid.removeEventListener('touchend', handleTouchEnd);
-    };
-  }, [images]);
 
   return (
     <div className="image-gallery">
@@ -199,8 +164,10 @@ const Gallery: React.FC = () => {
           <div
             key={image.id}
             draggable
-            data-image-id={image.id}
             onDragStart={(e) => handleDragStart(e, image)}
+            onTouchStart={(e) => handleTouchStart(e, image)}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
           >
             <ImageCard {...image} />
           </div>
